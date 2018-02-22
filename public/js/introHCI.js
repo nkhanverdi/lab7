59191c78f7dff92b014035caf3f61f20f32d7dae
Neda Khanverdi <nkhanver@ucsd.edu>
2018-02-22 09:26:42 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,13 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$('.likeBtn').click(buttonClicked);
 	// your code here
+}
+
+function buttonClicked(e) {
+	// create action
+	e.preventDefault();
+	ga('create', 'UA-114626968-1', 'auto');
+	ga('send', 'event', 'like', 'click');
 }
